d4065b21d46fed5ccabf60483f8a161da8877ea7
Harish-Joshi6367 <harisharma6367@gmail.com>
2024-10-28 07:46:13 +0530

    Update Login.jsx

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useAuth } from "../../authContext";
+import { useAuth } from ".././authContext";
 
 import { PageHeader, Token } from "@primer/react";
 import { Box, Button } from "@primer/react";
